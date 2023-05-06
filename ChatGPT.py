88092965923ba2efb2c925ec38acb625867d997e
markbang <3137972908@qq.com>
2023-05-06 21:24:47 +0800

    change

--- ChatGPT.py
+++
@@ -1,7 +0,0 @@
-import openai
-openai.api_key='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'
-completion=openai.ChatCompletion.create(
-    model='gpt-3.5-Turbo',
-    messages=[{'role':'assistant','content':'hello'}]
-)
-print(completion)
